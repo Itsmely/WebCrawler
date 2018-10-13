8eb2bc3680b0711c2ccc77ea0a66d22bcd6fae94
Itsmely队长 <heiandeyongtan@sina.com>
2018-10-13 23:05:20 +0800

    update

--- src/main/java/HttpClientPost.java
+++ src/main/java/HttpClientPost.java
@@ -1,30 +1,41 @@
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.util.EntityUtils;
 
 import java.io.IOException;
-import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
-public class HttpClientGet {
+public class HttpClientPost {
     public static void main (String[] args) throws Exception {
 
         CloseableHttpClient httpClient = HttpClients.createDefault();
 
-        //HttpGet httpGet = new HttpGet("https://www.baidu.com/s?wd=python");
-        //创建URI Builder设置参数
-        URIBuilder uriBuilder = new URIBuilder("https://sou.autohome.com.cn/zonghe").setParameter("q","golf");
-        URI uri = uriBuilder.build();
-        System.out.println(uri);
+        HttpPost httpPost = new HttpPost("https://www.oschina.net/");
+        //设置用户代理信息
+        httpPost.setHeader("User-Agent","");
 
-        HttpGet httpGet = new HttpGet(uri);
+        //创建List集合封装请求参数
+        List<NameValuePair> params = new ArrayList<NameValuePair>();
+        params.add(new BasicNameValuePair("scope","project"));
+        params.add(new BasicNameValuePair("q","python"));
+
+        //使用封装请求参数的List集合，创建表单实体对象
+        UrlEncodedFormEntity formEntity = new UrlEncodedFormEntity(params,"UTF-8");
+
+        //把表单实体设置到httpPost中
+        httpPost.setEntity(formEntity);
+
         CloseableHttpResponse response = null;
         try {
-             response = httpClient.execute(httpGet);
-
-            //System.out.println(response.getStatusLine().getStatusCode());
+             response = httpClient.execute(httpPost);
+            System.out.println(httpPost);
+            System.out.println(response.getStatusLine().getStatusCode());
             if(response.getStatusLine().getStatusCode() == 200){
                 String html = EntityUtils.toString(response.getEntity(),"UTF-8");
                 System.out.println(html);
